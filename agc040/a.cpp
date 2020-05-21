1e44a11ca00193e841492b623450def6203124af
yo-shi <yskshi007@gmail.com>
2020-05-21 10:29:52 +0900

    ac agc040-a

---
+++ agc040/a.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> P;
+
+#define all(v) v.begin(), v.end()
+
+const ll mod=1000000007;
+
+ll countBits(ll in){
+  int res=0;
+  for(;in>0;in>>=1){
+    if((in&0x01)!=0){
+      res++;
+    }
+  }
+  return res;
+}
+
+template<typename T>
+void show2dVector(vector<vector<T>>& v){
+  for(int i=0;i<v.size();i++){
+    int m=v[i].size();
+    cout<<i<<" : ";
+    for(int j=0;i<m;i++){
+      cout<<v[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+void bfs(const vector<vector<int>>&g, int v, vector<bool>& seen){
+  seen[v]=true;
+  cout<<v<<" ";
+  for(auto next: g[v]){
+    if(seen[next]){
+      continue;
+    }
+    bfs(g,next,seen);
+  }
+}
+
+bool dfs(vector<vector<int>> &g,int start,int goal,vector<bool>&seen){
+  bool res=false;
+  seen[start]=true;
+  if(start==goal){
+    return true;
+  }
+  for(auto next: g[start]){
+    if(seen[next]){
+      continue;
+    }
+    res=dfs(g,next,goal,seen);
+    if(res){
+      break;
+    }
+  }
+  return res;
+}
+
+ll gcd(ll a,ll b) {
+  return b? gcd(b,a%b): a;  
+}
+ll lcm(ll a,ll b) {
+  return a*b/gcd(a,b);
+}
+bool isLowerCase(char c){
+  return (c>='a'&&c<='z');
+}
+
+ll powm(ll a,ll n, ll m){
+  ll ret=1;
+  while(n>0){
+    if(n%2==1){
+      ret=(ret*a)%m;
+    }
+    n>>=1;
+    a=(a*a)%m;
+  }
+  return ret;
+}
+
+const string yesno(bool ans){
+  return (ans?"Yes":"No");
+}
+int main() {
+  string s;cin>>s;
+  ll ans=0;
+  int n=s.size()+1;
+  vector<int> left(n);
+  vector<int> right(n);
+  int cnt=0;
+  for(int i=0;i<n-1;i++){
+    if(s[i]=='<'){
+      cnt++;
+    }else{
+      cnt=0;
+    }
+    left[i+1]=cnt;
+    cerr<<cnt<<" \n"[i==n-2?1:0];
+  }
+  cnt=0;
+  for(int i=0;i<n-1;i++){
+    if(s[s.size()-1-i]=='>'){
+      cnt++;
+    }else{
+      cnt=0;
+    }
+    right[i+1]=cnt;
+    cerr<<cnt<<" \n"[i==n-2?1:0];
+  }
+  reverse(all(right));
+  for(int i=0;i<n;i++){
+    cerr<<left[i]<<" : "<<right[i]<<endl;
+    ans+=max(left[i],right[i]);
+  }
+  cout<<ans<<endl;
+  return 0;
+}
