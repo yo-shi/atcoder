f904e8f3b2da80ad21f063a4812a5cd13d668c51
yo-shi <yskshi007@gmail.com>
2020-06-06 22:53:36 +0900

    ac abc121 d

---
+++ abc121/d.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> P;
+
+#define all(v) v.begin(), v.end()
+
+const ll mod=1000000007;
+
+ll countBits(ll in){
+  int res=0;
+  for(;in>0;in>>=1){
+    if((in&0x01)!=0){
+      res++;
+    }
+  }
+  return res;
+}
+
+template<typename T>
+void show2dVector(vector<vector<T>>& v){
+  for(int i=0;i<v.size();i++){
+    int m=v[i].size();
+    cout<<i<<" : ";
+    for(int j=0;i<m;i++){
+      cout<<v[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+void bfs(const vector<vector<int>>&g, int v, vector<bool>& seen){
+  seen[v]=true;
+  cout<<v<<" ";
+  for(auto next: g[v]){
+    if(seen[next]){
+      continue;
+    }
+    bfs(g,next,seen);
+  }
+}
+
+bool dfs(vector<vector<int>> &g,int start,int goal,vector<bool>&seen){
+  bool res=false;
+  seen[start]=true;
+  if(start==goal){
+    return true;
+  }
+  for(auto next: g[start]){
+    if(seen[next]){
+      continue;
+    }
+    res=dfs(g,next,goal,seen);
+    if(res){
+      break;
+    }
+  }
+  return res;
+}
+
+ll gcd(ll a,ll b) {
+  return b? gcd(b,a%b): a;  
+}
+ll lcm(ll a,ll b) {
+  return a*b/gcd(a,b);
+}
+bool isLowerCase(char c){
+  return (c>='a'&&c<='z');
+}
+
+ll powm(ll a,ll n, ll m){
+  ll ret=1;
+  while(n>0){
+    if(n%2==1){
+      ret=(ret*a)%m;
+    }
+    n>>=1;
+    a=(a*a)%m;
+  }
+  return ret;
+}
+
+const string yesno(bool ans){
+  return (ans?"Yes":"No");
+}
+
+ll f(ll val){
+  ll res=0;
+  cerr<<"val = "<<val<<endl;
+  if(val==0){
+    return res;
+  }
+  for(int i=1;i<64;i++){
+    ll pow2=1LL<<i;
+    cerr<<"pow2 = "<<pow2<<endl;
+    if(val<pow2/2){
+      break;
+    }
+    ll biti=(val/pow2*(pow2/2)+max(0LL,val%pow2-(pow2/2-1)))%2;
+    res|=(biti<<(i-1));
+  }
+  cerr<<endl;
+  cerr<<"res = "<<res<<endl;
+  return res;
+}
+int main() {
+  ll a,b;cin>>a>>b;
+  ll ans=f(a-1)^f(b);
+  cout<<ans<<endl;
+  return 0;
+}
