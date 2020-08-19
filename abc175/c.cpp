c7e1365963df867733acdec6e5413f29da43e805
yo-shi <yskshi007@gmail.com>
2020-08-19 22:04:28 +0900

    ac abc175c

---
+++ abc175/c.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> P;
+
+#define all(v) v.begin(), v.end()
+
+const ll mod=1000000007;
+
+ll countBits(ll in){
+  int res=0;
+  for(;in>0;in>>=1){
+    if((in&0x01)!=0){
+      res++;
+    }
+  }
+  return res;
+}
+
+template<typename T>
+void show2dVector(vector<vector<T>>& v){
+  for(int i=0;i<v.size();i++){
+    int m=v[i].size();
+    cout<<i<<" : ";
+    for(int j=0;i<m;i++){
+      cout<<v[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+void bfs(const vector<vector<int>>&g, int v, vector<bool>& seen){
+  seen[v]=true;
+  cout<<v<<" ";
+  for(auto next: g[v]){
+    if(seen[next]){
+      continue;
+    }
+    bfs(g,next,seen);
+  }
+}
+
+bool dfs(vector<vector<int>> &g,int start,int goal,vector<bool>&seen){
+  bool res=false;
+  seen[start]=true;
+  if(start==goal){
+    return true;
+  }
+  for(auto next: g[start]){
+    if(seen[next]){
+      continue;
+    }
+    res=dfs(g,next,goal,seen);
+    if(res){
+      break;
+    }
+  }
+  return res;
+}
+
+ll gcd(ll a,ll b) {
+  return b? gcd(b,a%b): a;  
+}
+ll lcm(ll a,ll b) {
+  return a*b/gcd(a,b);
+}
+bool isLowerCase(char c){
+  return (c>='a'&&c<='z');
+}
+
+ll powm(ll a,ll n, ll m){
+  ll ret=1;
+  while(n>0){
+    if(n%2==1){
+      ret=(ret*a)%m;
+    }
+    n>>=1;
+    a=(a*a)%m;
+  }
+  return ret;
+}
+
+void primeFactor(ll val, map<ll,ll> &primeList){
+  if(val<1){
+    return;
+  }
+  for(int i=2;i*i<val;i++){
+    while(val%i==0){
+      primeList[i]++;
+      val/=i;
+    }
+  }
+  if(val!=1){
+    primeList[val]=1;
+  }
+  return;
+}
+
+const string yesno(bool ans){
+  return (ans?"Yes":"No");
+}
+ll near(ll val, ll d){
+  if(val<0){
+    return val+d;
+  }else{
+    return val-d;
+  }
+}
+
+ll far(ll val, ll d){
+  if(val<0){
+    return val-d;
+  }else{
+    return val+d;
+  }
+}
+int main() {
+  ll x,k,d;cin>>x>>k>>d;
+  ll ans=0;
+  ll div=abs(x)/d;
+  if(div<k){
+    x=near(x,d*div);
+    k-=div;
+    if(k%2==0){
+      ans=x;
+    }else{
+      ans=near(x,d);
+    }
+  }else{
+    ans=near(x,k*d);
+  }
+  cout<<abs(ans)<<endl;
+  return 0;
+}
